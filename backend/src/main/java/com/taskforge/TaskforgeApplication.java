d9902e86b609236ceba3f1221b5371b0e3fe4346
Supriya200508 <supriyadec08@gmail.com>
2026-03-31 13:54:06 +0530

    fix

--- backend/src/main/java/com/taskforge/TaskforgeApplication.java
+++ backend/src/main/java/com/taskforge/TaskforgeApplication.java
@@ -2,11 +2,15 @@ package com.taskforge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TaskforgeApplication {
 
 	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(TaskforgeApplication.class, args);
 	}
 
